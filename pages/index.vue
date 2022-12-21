48c1dc9cb82ae96b0cdec3d6a9793673bfcd6a7a
kanoi-y <furafuratonbo@gmail.com>
2022-12-21 17:59:58 +0900

    ✨ assetsとcomposableとpluginを使用

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,10 @@
 <template>
   <div>
     <h1>Main Page</h1>
-    <button @click="handleClick">Coupon Get</button>
-    <LazyCoupon v-if="show" />
+    <h2>{{ $hello('World') }}</h2>
   </div>
 </template>
-<script setup>
-const show = ref(false);
 
-const handleClick = () => {
-  show.value = true;
-};
+<script setup>
+const { $hello } = useNuxtApp();
 </script>
