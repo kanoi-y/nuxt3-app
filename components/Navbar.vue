7e0084a9e45b2ef5d715613236cccc8f6cc39168
kanoi-y <furafuratonbo@gmail.com>
2022-12-22 21:26:37 +0900

    ✨ DataFetchingを学習

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,5 +3,6 @@
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/about">About</NuxtLink>
     <NuxtLink :to="{ name: 'users-list' }">User List</NuxtLink>
+    <NuxtLink to="/posts">Post List</NuxtLink>
   </nav>
 </template>
