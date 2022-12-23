075181173998c71bf4dc2c9089a11c68390cc74c
kanoi-y <furafuratonbo@gmail.com>
2022-12-23 15:24:22 +0900

    ✨ エラーハンドリングを学習

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout>
+    <p>{{ error }}</p>
+    <button @click="clearError({ redirect: '/' })">Clear Error</button>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object,
+});
+</script>
--- pages/users/list.vue
+++ pages/users/list.vue
@@ -1,3 +1,17 @@
+<script setup>
+const { data: users } = await useFetch(
+  'https://jsonplaceholder.typicode.com/user/'
+);
+console.log('process');
+if (!users.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page Not Found' });
+}
+</script>
 <template>
-  <h1>User Listページ</h1>
+  <div>
+    <h1>ユーザリスト</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+  </div>
 </template>
