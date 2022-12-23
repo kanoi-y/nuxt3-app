ade6ad55bec00b0926070fe7a3d3e2bf3284c3ff
kanoi-y <furafuratonbo@gmail.com>
2022-12-23 15:46:32 +0900

    ✨ api route機能を学習

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,29 @@
+<script setup>
+const task = ref('');
+const { data: tasks, refresh } = useFetch('/api/task');
+
+const addTask = () => {
+  const { data } = useFetch('/api/task', {
+    method: 'post',
+    body: { task: task.value },
+  });
+  refresh();
+  task.value = '';
+};
+</script>
 <template>
   <div>
     <h1>Main Page</h1>
-    <h2>{{ $hello('World') }}</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">{{ task.task }}</li>
+    </ul>
+    <form @submit.prevent="addTask">
+      <div>
+        <input v-model="task" />
+      </div>
+      <div>
+        <button type="submit">タスクを登録</button>
+      </div>
+    </form>
   </div>
 </template>
-
-<script setup>
-const { $hello } = useNuxtApp();
-</script>
